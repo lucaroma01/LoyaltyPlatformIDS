ce750c82f5a7572d01aaedefb7498771fffd03e7
lucaroma01 <luca02.romagnoli@studenti.unicam.it>
2023-11-28 15:33:15 +0100

    "Creazione metodi payment(), addCarta(), visualizzaCartaDiCredito(), getByID(), nella classe ControllerPagamento"

--- api/src/main/java/it/unicam/cs/ids/loyaltyplatform/Controller/ControllerPagamento.java
+++ api/src/main/java/it/unicam/cs/ids/loyaltyplatform/Controller/ControllerPagamento.java
@@ -1,9 +1,13 @@
 package it.unicam.cs.ids.loyaltyplatform.Controller;
 
 import it.unicam.cs.ids.loyaltyplatform.Model.*;
+import it.unicam.cs.ids.loyaltyplatform.Services.DBMSController;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+
 public class ControllerPagamento {
 
     private TitolarePuntoVendita titolarePuntoVendita;
@@ -12,5 +16,48 @@ public class ControllerPagamento {
 
     public ControllerPagamento() {
         this.listaCartaDiCredito= new ArrayList<>();
+    }
+
+    /**
+     * se il pagamento è stato effettuato correttamente
+     *
+     * @return true , altrimenti false;
+     */
+
+    public boolean payment(TitolarePuntoVendita t) throws SQLException {
+        if (t.getCarta().getSaldoCarta()>GestorePiattaforma.getCostoAdesionePiattaforma()){
+            t.getCarta().decrementaSaldo(GestorePiattaforma.getCostoAdesionePiattaforma());
+            return true;
+        }
+        return false;
+    }
+
+    public void addCarta(CartaDiCredito cc) throws SQLException {
+        String query = "INSERT INTO cartadicredito (id_cc, scadenza_cc, cvv, pin, saldocarta) VALUES('" + cc.getNumeroCarta() + "','" + cc.getDateScadenza() + "','" + cc.getCVV() + "','" + cc.getPin() + "','" + cc.getSaldoCarta() + "')";
+        DBMSController.insertQuery(query);
+    }
+
+    public List<CartaDiCredito> visualizzaCartaDiCredito() throws SQLException, ErrorDate {
+        String table="cartadicredito";
+        ResultSet resultSet= DBMSController.selectAllFromTable(table);
+        while (resultSet.next()){
+            CartaDiCredito cc= new CartaDiCredito(resultSet.getInt("id_cc"),resultSet.getDate("scadenza_cc"),
+                    resultSet.getInt("cvv"),resultSet.getInt("pin"),
+                    resultSet.getDouble("saldocarta"));
+            this.listaCartaDiCredito.add(cc);
+        }
+        return this.listaCartaDiCredito;
+    }
+
+    public CartaDiCredito getByID(int id){
+        CartaDiCredito cartaCredito = null;
+        for (CartaDiCredito cc : listaCartaDiCredito) {
+            if (cc.getNumeroCarta() == id)
+                cartaCredito = cc;
+        }
+        if (cartaCredito == null) {
+            return null;
+        }
+        return cartaCredito;
     }
 }
