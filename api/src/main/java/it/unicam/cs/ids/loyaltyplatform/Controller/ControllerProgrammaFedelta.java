23207bf8a8177a4ac58818f162e1f8f67c83af52
Alessandro <alessandro.canuti@studenti.unicam.it>
2023-12-01 11:56:09 +0100

    Fixato addProgrammaFedelta in ControllerProgrammaFedelta

--- api/src/main/java/it/unicam/cs/ids/loyaltyplatform/Controller/ControllerProgrammaFedelta.java
+++ api/src/main/java/it/unicam/cs/ids/loyaltyplatform/Controller/ControllerProgrammaFedelta.java
@@ -23,7 +23,7 @@ public class ControllerProgrammaFedelta {
         listaProgrammi.add(programmaFedelta);
         String query = "";
         if(programmaFedelta instanceof ProgrammaPunti programmaPunti) {
-            query = "INSERT INTO programmapunti (id_pp, nome_pp, descrizione_pp, importocostantexpunti, totpunti) VALUES('" + programmaPunti.getId() + "', '" + programmaPunti.getNome() + "', '" + programmaPunti.getDescrizione() + "', '" + programmaPunti.getImportoXCostantePunti() + "', '" + programmaPunti.getTotPunti() + "')";
+            query = "INSERT INTO programmapunti (id_pp, nome_pp, descrizione_pp, importoxcostantepunti, totpunti) VALUES('" + programmaPunti.getId() + "', '" + programmaPunti.getNome() + "', '" + programmaPunti.getDescrizione() + "', '" + programmaPunti.getImportoXCostantePunti() + "', '" + programmaPunti.getTotPunti() + "')";
         }
         if(programmaFedelta instanceof ProgrammaLivelli programmaLivelli) {
             query = "INSERT INTO programmalivelli (id_pl, nome_pl, descrizione_pl, percentualelivelloximporto, puntilivello, livellomax ) VALUES('" + programmaLivelli.getId() + "', '" + programmaLivelli.getNome() + "', '" + programmaLivelli.getDescrizione() + "', '" + programmaLivelli.getPercentualeLivelloXImporto() + "', '" + programmaLivelli.getPuntiLivello()+ "', '" + programmaLivelli.getLivelloMax() + "')";
